efdc01c56cd4a72b9f971b8e0001849c1814684c
hemarastylepeke <hemarastylpeke@gmail.com>
2023-06-21 23:21:07 +0300

    first deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn voltage_labs.wsgi --bind 0.0.0.0:$PORT --workers 4
--- voltage_tailwind/static_src/tailwind.config.js
+++ voltage_tailwind/static_src/tailwind.config.js
@@ -73,4 +73,5 @@ module.exports = {
         require('@tailwindcss/line-clamp'),
         require('@tailwindcss/aspect-ratio'),
     ],
+    darkMode: 'class',
 }
