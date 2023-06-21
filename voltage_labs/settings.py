0381333a7c06a237e17d312bdac00ff9f8c5895e
hemarastylepeke <hemarastylpeke@gmail.com>
2023-06-21 23:45:43 +0300

    third deployment

--- voltage_labs/settings.py
+++ voltage_labs/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-p-t+o45im!_+t==fpnc!!!r@yp((s!6=%*#1t4oeh3q$@d6e#*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["e5129945-7eac-43bb-971f-82a7b3775d7d"]
 
